86e70f5cadb1ccaeb2da2b14c8208c222773c427
jerrychen <jerrychen040@gmail.com>
2016-12-14 10:44:33 +0800

    build detail vote results views function to accept the question_id parameter from url

--- mysite/my_polls_app/urls.py
+++ mysite/my_polls_app/urls.py
@@ -7,12 +7,31 @@ from . import views
 # ok, we can define what url pattern need to match to what method on views.py
 # that is why we need import views
 
+'''
+1.這裡的  url  指的就是瀏覽器收到的url, 不論是什麼樣子
+2.而括號內參數一就是對這個url 擷取, 拿出你要的參數, 然後參數二就是導向去其他views
+'''
+
 urlpatterns = [
     # views.index = (file name).(function name)
+    # url = 127.0.0.1:8000/my_polls_app/
     url(r'^$', views.index_my, name='index'),
     # r'^$' means don't change anything from the url
     # which means url here equal to  127.0.0.1:8000/my_polls_app/
     # and if so, I want to direct to views.index function
     # and we give the url a name (index)
+
+    # for detail page
+    # 因為你要傳入 url/1/ 當中的 1 , 塞給 question_id 丟去views
+    # url = 127.0.0.1:8000/my_polls_app/2
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # for vote page
+    # url = 127.0.0.1:8000/my_polls_app/2/vote
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    # for results page
+    # url = 127.0.0.1:8000/my_polls_app/2/results
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 
 ]
--- mysite/my_polls_app/views.py
+++ mysite/my_polls_app/views.py
@@ -1,8 +1,50 @@
 from django.shortcuts import render
-
+# 每個function 代表你想要呈現某種 頁面
+# 因為你可以預期, 每個function 最後要return 回給request 一個 html page, 這樣
+# 人輸入的某個網址 , 才會得到這個html return, 才會有東西可以看
 
 from django.http import HttpResponse
 # Create your views here.
 def index_my(request):
     print("Get it")
     return HttpResponse("This is index function in views.py, which called by http://127.0.0.1:8000/my_polls_app/")
+
+
+'''
+所以關於設計上:
+上面有個 index 是基本上合理的, 就是首頁
+假設我想在首頁列出""所有""=>我們準備好的問題
+
+
+那針對這個 問券app, 其實還有幾個你早就猜想的到的頁面需要做
+Page 1.
+如果有人點進去首頁的某個問題, 他想要回答, 則我們需要一個 針對
+"這個問題"的 detail 頁面.
+a.去顯示,提供出來"某個指定問題"的預設選項 (某個指定的問題-> 想必需要一個question id)
+b.讓他可以對那些選項投票
+
+Page 2.
+按下投票後該出現的頁面
+
+Page 3.
+可以顯示出某個問題截至目前為止的票數總和
+'''
+
+# for page1: detail
+'''
+PS1:
+function 帶入的參數一定要有 request (畢竟就是要服務http request), 後面要再帶其他參數 就看你在當初request 後面有沒有帶進來
+有需要就自己帶, 跟一般寫code 一樣
+PS2:
+記得去 urls.py 加這個路由進來
+'''
+def detail(request,question_id):
+    return HttpResponse("This is detail view, called by http://127.0.0.1:8000/my_polls_app/%s/"%question_id)
+
+#for page 2: vote
+def vote(request,question_id):
+    return HttpResponse("This is vote view, called by http://127.0.0.1:8000/my_polls_app/%s/vote/"%question_id)
+
+# for page3 : results
+def results(request,question_id):
+    return HttpResponse("This is results view, called by http://127.0.0.1:8000/my_polls_app/%s/results/"%question_id)
